9514332a3593f8aeada90059e498cd92445e87df
Vincent Daranyi <vincent@daranyi.com>
2015-03-13 20:14:16 -0400

    Content script update

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -9,7 +9,7 @@ var hnOrange = '#ff6600',
     commentersBgColor = hnOrange,
     bgGrey = "#f7f7f1",
     following = ['rickdale','colechristensen','jseliger','annbabe','mathouc','j2kun','concise','taejo','cpursley','mavdi', 'nkozyra', 'kazinator','comex','robin_reala','revscat','jakke','aestetix', 'ymmt2005', 'epetre', 'Graham24', 'datascientist', 'danso', 'gsans', 'adventured', 'epetre'],
-    getCommentersRoute = 'http://hn-select.herokuapp.com/getCommenters';
+    getCommentersRoute = 'https://hn-select.herokuapp.com/getCommenters';
 
 
 // Selecting highlighting method depending on view
@@ -62,7 +62,7 @@ function highlightNews() {
       }
       // Manipulate DOM with highlights
       highlightFollowing(storiesOnPage);
-    // });
+    });
 
   function highlightFollowing(storiesOnPage) {
     for (var s = 0; s < storiesOnPage.length; s++) {
