caa6a3b895212f33d364962e3db0a10e24e67460
Vincent Daranyi <vincent@daranyi.com>
2015-03-25 13:01:08 -0400

    Front-back fully working

--- scripts/compiled/bundle.js
+++ scripts/compiled/bundle.js
@@ -259,109 +259,10 @@ chrome.runtime.onMessage.addListener(
 
 
 },{"./sidebar.jsx":2}],2:[function(require,module,exports){
-// Fake data
+// Constants
 
 var server = 'http://localhost:3000';
 
-
-var newsFeedPlaceholder = [
-    {
-        type: "comment",
-        title: null,
-        url: null,
-        score: null,
-        by: "erbdex",
-        timestamp: "1427125337000",
-        text: "You win the internet sir. Bravo.",
-        commenton: "Two Cake made of pure awesomeness",
-        parent_poster: "You",
-        no_of_comments: 43
-    },
-    {
-        type: "news",
-        title: "Images that fool computer vision raise security concerns",
-        url: "cornell.edu",
-        score: null,
-        by: "lm60",
-        timestamp: "1427124617000",
-        text: "Computers are learning to recognize objects with near-human ability. But Cornell researchers have found that computers, like humans, can be fooled by optical illusions, which raises security concerns and opens new avenues for research in computer vision.\nCornell graduate student Jason Yosinski and colleagues at the University of Wyoming Evolving Artificial Intelligence Laboratory have created images that look to humans like white noise or random geometric patterns but which computers identify with great confidence as common objects. They will report their work at the IEEE Computer Vision and Pattern Recognition conference in Boston June 7-12.",
-        commenton: null,
-        parent_poster: null,
-        no_of_comments: 54
-    },
-    {
-        type: "comment",
-        title: null,
-        url: null,
-        score: null,
-        by: "lm60",
-        timestamp: "1427123297000",
-        text: "While I largely agree with you, it strikes me that you've missed an option -- It might be that there's something inefficient in the trial process that holds no weight on \"fairness\". I'm not a legal expert by any means, so it may not be the case, but it seems as though it's a possibility.",
-        commenton: "Amtrak police use of passenger data",
-        parent_poster: "CapitalistCartr",
-        no_of_comments: 91
-    },
-    {
-        type: "comment",
-        title: null,
-        url: null,
-        score: null,
-        by: "lm60",
-        timestamp: "1427122517000",
-        text: "This always frustrates me when discussions of plea bargaining and the right to trial come up, and the argument is given that plea bargaining is a necessity because the courts would be horribly overloaded if every case went to trial. If the system doesn't have the resources to give every accused criminal a fair trial, then either you're making too many criminals, the system doesn't have enough resources, or both. Bypassing trials is just a way to cover your ears and shout \"la la la\" to ignore the problem.",
-        commenton: "Amtrak police use of passenger data",
-        parent_poster: "msandford",
-        no_of_comments: 91
-    },
-    {
-        type: "news",
-        title: "Early posts from Larry Page, Linus Torvalds, Jan Koum, and more",
-        url: "carlcheo.com",
-        score: null,
-        by: "carlcheo",
-        timestamp: "1427121797000",
-        text: "Here’s a list of early posts from tech founders who used to ask questions, self-promote, and interact on forums and discussion groups. Just like us. Fascinating? Yes. I see passion too.\n#1. Google (1996) – When developing Google, Larry Page posted a Java question about setting User-Agent header for his web crawler. Even the smartest people have questions too. Let me Google the answer for you, Larry.",
-        commenton: null,
-        parent_poster: null,
-        no_of_comments: 145
-    },
-    {
-        type: "comment",
-        title: null,
-        url: null,
-        score: null,
-        by: "lm60",
-        timestamp: "1427121497000",
-        text: "If everyone is breaking so many laws that the police and courts can't keep up it doesn't mean that humanity is broken. It means that the law has gotten so far out of sync with humanity that the law is broken. People make the laws, not the other way around. The world would be a much better place if more people realized this.",
-        commenton: "Amtrak police use of passenger data",
-        parent_poster: "msandford",
-        no_of_comments: 91
-    },
-    {
-        type: "news",
-        title: "How to save datetimes for future events",
-        url: "creativedeletion.com",
-        score: null,
-        by: "laut",
-        timestamp: "1427120717000",
-        text: "Imagine that it’s January 2015 and you’re making an appointment in a calendar application for a meeting that will take place in Santiago, Chile on the April 30th at 10:00 in the morning.\nYour calendar software saves the appointment and you can see that it’s there with the description that you made. 2015-04-30 10:00 in Chile. You even checked the box to get a reminder and think to yourself: “What a time to be alive”. Software can remind us of meetings and keep track of timezones and we no longer have to carry around big bulky paper calendars.",
-        commenton: null,
-        parent_poster: null,
-        no_of_comments: 23
-    },
-    {
-        type: "news",
-        title: "Canada's CSE cyberwarfare toolbox revealed",
-        url: "cbc.ca",
-        score: null,
-        by: "colinprince",
-        timestamp: "1427119937000",
-        text: "Online gambling, to a cash-hungry province, must look like a ripe piece of fruit just waiting to be plucked.\nThe logic is easy to see. Gambling on the internet is already happening, so why shouldn't a province get a piece of the action rather than watch the money go elsewhere? What's more, government oversight can keep players safe from shady offshore operators, and protect problem gamblers from themselves by promoting responsible gaming practices.\nOntario used those arguments before jumping into internet gambling in January. Quebec, B.C., and Manitoba are equally well versed in the rationale and Atlantic Canada's gambling overseer is using it right now in a bid to expand the menu of online gaming options available on the East Coast.",
-        commenton: null,
-        parent_poster: null,
-        no_of_comments: 86
-    },
-]
 
 //==========================================================
 // Sidebar container and slider functionality
@@ -369,7 +270,7 @@ var newsFeedPlaceholder = [
 // Attaches an empty div to the DOM to which we can attach our React code
 $(document).ready(function () {
     $("body").append("<div id='sidebar-anchor'></div>");
-    React.render(React.createElement(SidebarBox, {data: newsFeedPlaceholder}), $("#sidebar-anchor").get(0));
+    React.render(React.createElement(SidebarBox, null), $("#sidebar-anchor").get(0));
 });
 
 // Sidebar component
@@ -596,7 +497,7 @@ var ContentList = React.createClass({displayName: "ContentList",
 
     getInitialState: function(){
         return {
-            data: []
+            data: null
         }
     },
 
@@ -608,25 +509,32 @@ var ContentList = React.createClass({displayName: "ContentList",
                 url: server + '/vdaranyi/newsfeed',
                 data: ''
             }, function(response) {
-                var newsfeed = JSON.parse(response);
-                console.log(newsfeed);
-                self.setState({data: newsfeed});
+                if (response) {
+                    var newsfeed = JSON.parse(response);
+                    self.setState({data: newsfeed});   
+                } else {
+                    self.setState({ data: null });
+                }
         })  
     },
 
     render: function () {
-        // Determine whether data object is a comment or a news article and render accordingly
-        return (
-            React.createElement("div", null, 
-                this.state.data.map(function (item) {
-                    return React.createElement(ContentItem, {data: item})
-                })
-            )
-        ) 
+        if (this.state.data) {
+            return (
+                React.createElement("div", null, 
+                    this.state.data.map(function (item) {
+                        return React.createElement(ContentItem, {data: item})
+                    })
+                )
+            ) 
+        } else {
+            return React.createElement("h6", {className: "content-maintitle"}, "Could not retrieve data from server");
+        } 
     }
 });
 
 var ContentItem = React.createClass({displayName: "ContentItem",
+    // Determine whether data object is a comment or a news article and render accordingly
     render: function(){
         if (this.props.data.type === "story") {
             return (
@@ -652,7 +560,7 @@ var ContentItem = React.createClass({displayName: "ContentItem",
                         React.createElement("h4", {className: "content-secondarytitle"}, " by ", this.props.data.by)
                     ), 
                     React.createElement("div", {className: "content-content"}, 
-                        React.createElement("p", {className: "content-text"}, this.props.data.text)
+                        React.createElement("p", {className: "content-text", dangerouslySetInnerHTML: {__html: this.props.data.text}})
                     ), 
                     React.createElement("div", {className: "content-footer"}, 
                         React.createElement("p", {className: "content-footertext"}, "original post by ", this.props.data.parent_poster, " ", timeToNow(this.props.data.timestamp), " | ", this.props.data.no_of_comments, " comments")
