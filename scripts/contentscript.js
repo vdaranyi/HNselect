906e892ee394db65ab548a1b671aeac40c82eef2
Vincent Daranyi <vincent@daranyi.com>
2015-04-06 22:01:05 -0400

    google analytics

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -6,6 +6,20 @@ require('./pageHighlighting.js');
 
 // FILE TO BE CLEANED UP
 
+// Google Analytics
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-61604728-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  console.log('GAQ:',_gaq);
+})();
+
+
 // Constants
 var hnOrange = '#ff6600',
     commentsBgColor = hnOrange,
